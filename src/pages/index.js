a3630b6e62abed012df9e9ce98ff1b444076b19d
Aurelia Beck <aurelia.beck@liganova.com>
2024-11-05 14:06:35 +0100

    add image preload functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,35 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import { useEffect } from 'react';
+
+
 
 export default function Home() {
+
+  
+    useEffect(() => {
+      // all images to preload
+      const imageUrls = [
+        '/indexBackground.png',
+        '/prequestionBackground.png',
+        '/questionBackground.png',
+        '/waitingBackground.png',
+        '/resultsBackground.png',
+        '/explainationBackground.png',
+        '/cardsBackground.png',
+        '/buttonBackground.png',
+        '/disclaimerBackground.png',
+        '/backcard.jpg'
+      ];
+  
+      // function to preload
+      imageUrls.forEach((url) => {
+        const img = new Image();
+        img.src = url;
+      });
+    }, []);
+
+
   return (
     <div className={styles.container}>
       <div className={styles.background}>
